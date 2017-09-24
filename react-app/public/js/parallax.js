01dccd99be9347f0993c034b174b679c49bf6bd2
Ben Watson <americk0@gmail.com>
2017-09-24 16:35:07 -0400

    in progress

--- react-app/public/js/parallax.js
+++ react-app/public/js/parallax.js
@@ -3,10 +3,14 @@
     var rsvpbgElement = document.querySelector('.rsvpbg');
     var rsvpbgPosition = rsvpbgElement.getBoundingClientRect().top - document.body.getBoundingClientRect().top;
     var heroElement = document.querySelector('#hero');
-    var heroPosition = heroElement.getBoundingClientRect().top - document.body.getBoundingClientRect().top;
+    var heroImgURL = heroElement.style['background-image'];
+    var heroRect = document.getBoundingClientRect();
+    var heroPosition = heroRect.top - document.body.getBoundingClientRect().top;
+    var heroHeight = heroRect.bottom - heroRect.top;
+    var heroWidth = heroRect.right - heroRect.left;
     window.addEventListener('scroll', function() {
       rsvpbgElement.style['background-position-y'] = ((window.scrollY - rsvpbgPosition) * .5) + 'px';
-      heroElement.style['background-position-y'] = ((window.scrollY - heroPosition) * .5) + 'px';
+      heroElement.style['background-position-y'] = ((window.scrollY - heroPosition) * .5) - ((heroWidth * h/w) - heroHeight) + 'px';
     });
   });
 })()
